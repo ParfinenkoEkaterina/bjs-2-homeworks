158f26e6c71bb67bc3934df3a8fa7e80e7469018
Ekaterina Parfinenko <katyavasilenko2012@gmail.com>
2023-09-29 15:51:22 +0300

    the code is formatted

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,19 +1,19 @@
 "use strict"
 
 function solveEquation(a, b, c) {
-  let arr = [];
-  let d = (b ** 2) - (4 * a * c);
-        if (d < 0) {
-                arr.push();
-        } else if (d == 0) {
-                let root = -b / (2 * a);
-                      arr.push(root);
-        } else if (d > 0) {
-                let firstSolution = (-b + Math.sqrt(d) )/(2 * a);
-               let secondSolution = (-b - Math.sqrt(d) )/(2 * a);
-                      arr.push(firstSolution, secondSolution); 
-        }
-  return arr;
+	let arr = [];
+	let d = (b ** 2) - (4 * a * c);
+	if (d < 0) {
+		arr.push();
+	} else if (d == 0) {
+		let root = -b / (2 * a);
+		arr.push(root);
+	} else if (d > 0) {
+		let firstSolution = (-b + Math.sqrt(d)) / (2 * a);
+		let secondSolution = (-b - Math.sqrt(d)) / (2 * a);
+		arr.push(firstSolution, secondSolution);
+	}
+	return arr;
 }
 
 
