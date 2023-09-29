2c7ee06cf99bdedc85a4ba8f7762232caf1ef527
Ekaterina Parfinenko <katyavasilenko2012@gmail.com>
2023-09-29 15:44:37 +0300

    Реализовано первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,9 +1,21 @@
 "use strict"
+
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = (b ** 2) - (4 * a * c);
+        if (d < 0) {
+                arr.push();
+        } else if (d == 0) {
+                let root = -b / (2 * a);
+                      arr.push(root);
+        } else if (d > 0) {
+                let firstSolution = (-b + Math.sqrt(d) )/(2 * a);
+               let secondSolution = (-b - Math.sqrt(d) )/(2 * a);
+                      arr.push(firstSolution, secondSolution); 
+        }
   return arr;
 }
+
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
