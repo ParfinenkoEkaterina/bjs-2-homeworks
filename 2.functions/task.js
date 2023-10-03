b975f73dac9efdb460010b4a9dc695fedb626eef
Ekaterina Parfinenko <katyavasilenko2012@gmail.com>
2023-10-03 14:26:18 +0300

    Первая задача выполнена

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,8 +1,28 @@
+//Задача 1
 function getArrayParams(...arr) {
+	let min = Infinity;
+	let max = -Infinity;
+	let sum = 0;
+	let avg = 0;
+	for (let i = 0; i < arr.length; i++) {
+		sum = sum + arr[i];
+		if (arr[i] > max) {
+			max = arr[i];
+		}
+		if (arr[i] < min) {
+			min = arr[i];
+		}
+	}
+	avg = average((sum / arr.length).toFixed(2));
 
-  return { min: min, max: max, avg: avg };
+	return {
+		min: min,
+		max: max,
+		avg: avg
+	};
 }
 
+//Задача 2
 function summElementsWorker(...arr) {
 
 }
@@ -19,6 +39,7 @@ function averageEvenElementsWorker(...arr) {
 
 }
 
+//Задача 3
 function makeWork (arrOfArr, func) {
 
 }
