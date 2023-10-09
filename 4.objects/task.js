0a6dd9246c8f37a409f06b9cd937636aadd5b7da
Ekaterina Parfinenko <katyavasilenko2012@gmail.com>
2023-10-09 15:55:58 +0300

    исправление внесены

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,32 +1,32 @@
 function Student(name, gender, age) {
-    this.name = name;
-    this.gender = gender;
-    this.age = age;
-    this.marks = [];
-}
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 
-Student.prototype.setSubject = function (subjectName) {
-    this.subject = subjectName; 
-}
+	this.setSubject = function(subjectName) {
+		this.subject = subjectName;
+	};
 
-Student.prototype.addMarks = function (...marks) {
-    if (!this.marks) {
-        console.log("Error: Student has been expelled.");
-        return;
-    }
-    this.marks.push(...marksToAdd);
-}
+	this.addMarks = function(...marksToAdd) {
+		if (!this.marks) {
+			console.log("Error: Student has been expelled.");
+			return;
+		}
+		this.marks.push(...marksToAdd);
+	};
 
-Student.prototype.getAverage = function () {
-    if (!this.marks || !this.marks.length) {
-        return 0;
-    }
-    const sum = this.marks.reduce((acc, curr) => acc + curr);
-    return sum / this.marks.length;
-}
+	this.getAverage = function() {
+		if (!this.marks || !this.marks.length) {
+			return 0;
+		}
+		const sum = this.marks.reduce((acc, curr) => acc + curr);
+		return sum / this.marks.length;
+	};
 
-Student.prototype.exclude = function (reason) {
-    delete this.subject;
-    delete this.marks;
-    this.excluded = reason;
+	Student.prototype.exclude = function(reason) {
+		delete this.subject;
+		delete this.marks;
+		this.excluded = reason;
+	};
 }
